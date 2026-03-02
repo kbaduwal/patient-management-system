30b3d58adbd7be65505215405a0bc7206d938992
kbaduwal <khakendra1999@gmail.com>
2026-03-02 23:53:50 +0000

    uncommited file

--- infrastructure/src/main/java/com/pm/stack/LocalStack.java
+++ infrastructure/src/main/java/com/pm/stack/LocalStack.java
@@ -1,7 +1,32 @@
 package com.pm.stack;
 
-import software.amazon.awscdk.Stack;
+import com.amazonaws.services.ec2.model.Vpc;
+import software.amazon.awscdk.*;
 
 public class LocalStack extends Stack {
+    private final software.amazon.awscdk.services.ec2.Vpc vpc;
 
+    public LocalStack(final App scope, final String id, final StackProps props){
+
+        super(scope, id, props);
+        this.vpc = createVpc();
+    }
+    private Vpc createVpc(){
+        return  Vpc.Builder
+                .create(this,"PatientManagementVPC")
+                .vpcName("PatientManagementVPC")
+                .maxAzs(2)
+                .build();
+    }
+
+    public static void main(final String[] args) {
+        App app = new App(AppProps.builder().outdir("./cdk.out").build());
+        StackProps props = StackProps.builder()
+                .synthesizer(new BootstraplessSynthesizer())
+                .build();
+
+        new LocalStack(app,"localstack",props);
+        app.synth();
+        System.out.println("App synthesizing in progress...");
+    }
 }
